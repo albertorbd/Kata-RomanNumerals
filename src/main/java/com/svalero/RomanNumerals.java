1adb11f6212db9eec9fa936e5c94d194d40cddf1
albertorbd <albertiriveiro@gmail.com>
2023-05-22 16:32:14 +0200

    Sixth Commit
    Creamos un método que nos permite pasar del 1 al 100 a números romanos simplificando el código

--- src/main/java/com/svalero/RomanNumerals.java
+++ src/main/java/com/svalero/RomanNumerals.java
@@ -1,40 +1,21 @@
 package com.svalero;
 
 public class RomanNumerals {
-    public static String convertRoman(int entero) {
-        String romanNumber = null;
 
-        if (entero == 1) {
-            romanNumber = "I";
+    private static final String[] Units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    private static final String[] Tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static final String[] Hundreds = {"", "C"};
+    public static String convertRoman(int entero) {
+        if (entero < 1 || entero > 100) {
+            throw new IllegalArgumentException("El número debe estar entre 1 y 100");
         }
-        if (entero == 2) {
-            romanNumber = "II";
-        }
-        if (entero == 3) {
-            romanNumber = "III";
-        }
-        if (entero == 4) {
-            romanNumber = "IV";
-        }
-        if (entero == 5) {
-            romanNumber = "V";
-        }
-        if (entero == 6) {
-            romanNumber = "VI";
-        }
-        if (entero == 7) {
-            romanNumber = "VII";
-        }
-        if (entero == 8) {
-            romanNumber = "VIII";
-        }
-        if (entero == 9) {
-            romanNumber = "IX";
-        }
-        if (entero == 10) {
-            romanNumber = "X";
-        }
-        return romanNumber;
+
+        int unit = entero % 10;
+        int ten = (entero / 10) % 10;
+        int hundred = entero / 100;
+
+        return Hundreds[hundred] + Tens[ten] + Units[unit];
+
     }
 
 }
--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -20,6 +20,18 @@ public class RomanNumeralsShould {
         Assert.assertEquals("VII", romanNumerals.convertRoman(7));
         Assert.assertEquals("VIII", romanNumerals.convertRoman(8));
         Assert.assertEquals("IX", romanNumerals.convertRoman(9));
-        Assert.assertEquals("X", romanNumerals.convertRoman(10));
+        Assert.assertEquals("XII", romanNumerals.convertRoman(12));
+        Assert.assertEquals("XV", romanNumerals.convertRoman(15));
+        Assert.assertEquals("XVII", romanNumerals.convertRoman(17));
+        Assert.assertEquals("XXI", romanNumerals.convertRoman(21));
+        Assert.assertEquals("XXV", romanNumerals.convertRoman(25));
+        Assert.assertEquals("XXXII", romanNumerals.convertRoman(32));
+        Assert.assertEquals("XLIV", romanNumerals.convertRoman(44));
+        Assert.assertEquals("LII", romanNumerals.convertRoman(52));
+        Assert.assertEquals("LXVII", romanNumerals.convertRoman(67));
+        Assert.assertEquals("LXXVIII", romanNumerals.convertRoman(78));
+        Assert.assertEquals("LXXXVI", romanNumerals.convertRoman(86));
+        Assert.assertEquals("C", romanNumerals.convertRoman(100));
+
     }
 }
