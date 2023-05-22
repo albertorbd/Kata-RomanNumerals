8371d8983587c75b9fdcc94843a0ae778dd21d51
albertorbd <albertiriveiro@gmail.com>
2023-05-22 16:15:31 +0200

    Fifth Commit
    Hacer la conversión hasta el número 10 siguiendo el mismo proceso

--- src/main/java/com/svalero/RomanNumerals.java
+++ src/main/java/com/svalero/RomanNumerals.java
@@ -10,6 +10,30 @@ public class RomanNumerals {
         if (entero == 2) {
             romanNumber = "II";
         }
+        if (entero == 3) {
+            romanNumber = "III";
+        }
+        if (entero == 4) {
+            romanNumber = "IV";
+        }
+        if (entero == 5) {
+            romanNumber = "V";
+        }
+        if (entero == 6) {
+            romanNumber = "VI";
+        }
+        if (entero == 7) {
+            romanNumber = "VII";
+        }
+        if (entero == 8) {
+            romanNumber = "VIII";
+        }
+        if (entero == 9) {
+            romanNumber = "IX";
+        }
+        if (entero == 10) {
+            romanNumber = "X";
+        }
         return romanNumber;
     }
 
--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -13,5 +13,13 @@ public class RomanNumeralsShould {
         RomanNumerals romanNumerals = new RomanNumerals();
         Assert.assertEquals("I", romanNumerals.convertRoman(1));
         Assert.assertEquals("II", romanNumerals.convertRoman(2));
+        Assert.assertEquals("III", romanNumerals.convertRoman(3));
+        Assert.assertEquals("IV", romanNumerals.convertRoman(4));
+        Assert.assertEquals("V", romanNumerals.convertRoman(5));
+        Assert.assertEquals("VI", romanNumerals.convertRoman(6));
+        Assert.assertEquals("VII", romanNumerals.convertRoman(7));
+        Assert.assertEquals("VIII", romanNumerals.convertRoman(8));
+        Assert.assertEquals("IX", romanNumerals.convertRoman(9));
+        Assert.assertEquals("X", romanNumerals.convertRoman(10));
     }
 }
