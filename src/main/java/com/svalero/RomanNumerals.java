05812c2c464b650f983e088dc2e96a72244216a1
albertorbd <albertiriveiro@gmail.com>
2023-05-25 12:58:30 +0200

    Linter improvement 3
    Seguimos mejorando el código

--- src/main/java/com/svalero/RomanNumerals.java
+++ src/main/java/com/svalero/RomanNumerals.java
@@ -3,33 +3,45 @@ package com.svalero;
 //Clase que convierte los números enteros a romanos
 
 public final class RomanNumerals {
+    //Unidades
 
-    private static final String[] UNITS = {"", "I", "II", "III", "IV", "V", "VI",
-            "VII", "VIII", "IX"};
+    private static final String[] UNITS = {"", "I", "II", "III", "IV",
+            "V", "VI", "VII", "VIII", "IX"};
+
+    //Decenas
     private static final String[] TENS = {"", "X", "XX", "XXX", "XL",
             "L", "LX", "LXX", "LXXX", "XC"};
+    //Centenas
     private static final String[] HUNDREDS = {"", "C", "CC", "CCC",
             "CD", "D", "DC", "DCC", "DCCC", "CM"};
 
+    //Millares
     private static final String[] THOUSANDS = {"", "M", "MM", "MMM"};
 
+    private RomanNumerals() {
 
-    public static String convertRoman( final int entero) {
+    }
+        // Constructor privado para evitar la instanciación de la clase
+        private static final int TEN = 10; // Nueva constante para representar el valor "10"
+        private static final int HUNDRED = 100; // Nueva constante para representar el valor "10"
+        private static final int THOUSAND = 1000; // Nueva constante para representar el valor "10"
 
-        int diez = 10;
+    public static String convertRoman(final int entero) {
+
         int cien = 100;
         int mil = 1000;
         if (entero < 1 || entero > 3000) {
-            throw new IllegalArgumentException("El número debe estar entre 1 y 3000");
+            throw new IllegalArgumentException("El número debe estar " +
+                    "entre 1 y 3000");
         }
 
-       final int unit = entero % diez;
-       final int ten = (entero / diez) % diez;
-       final int hundred = (entero / cien) % diez;
-       final int thousand = entero / mil;
+       final int unit = entero % TEN;
+       final int ten = (entero / TEN) % TEN;
+       final int hundred = (entero / HUNDRED) % TEN;
+       final int thousand = entero / THOUSAND;
 
-        return THOUSANDS[thousand] + HUNDREDS[hundred] +
-                TENS[ten] + UNITS[unit];
+        return THOUSANDS[thousand] + HUNDREDS[hundred]
+                +TENS[ten] + UNITS[unit];
 
     }
 
